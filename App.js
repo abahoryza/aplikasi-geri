bb377e66d98a9328c0a7fbc2799f24dc2090121c
Geri Sugiran <abah.oryza@gmail.com>
2019-01-07 20:58:41 +0700

    commit perdana

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  Button,
+  Input,
+  ThemeProvider
+} from 'react-native-elements';
+
+const theme = {
+  Button: {
+    titleStyle: {
+      color: 'white',
+    },
+  },
+};
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <ThemeProvider theme={theme}>
+
+        <Input placeholder='Username' />
+        <Button title = "Hey!" />
+
+      </ThemeProvider>
+    );
+  }
+}
